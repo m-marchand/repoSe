a30a50b3a7967f8014a54810551911473d24b4ac
Matthew <matthew.marchand.93@gmail.com>
2021-07-05 10:52:21 -0700

    successful github API query using octokit package

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
-import React, { Component } from 'react';
-// import top level component
+import React, { useState } from 'react';
+import { Octokit } from "@octokit/core";
+const octokit = new Octokit();
 
-class App extends Component {
-  render() {
-    return(
-      <div className='App'>
-        Hello World
-      </div>
-    );
+const App = () => {
+  
+  const [input, setInput] = useState('');
+  const [queryResult, setQueryResult] = useState([]);
+  // state: flag to trigger conditional render of detailed result page
+  // passing down pertinent query results as props
+
+  const searchSubmit = () => {
+    /*
+      sort results by default best match or by stars
+      filter results by language
+      in:name --> search repo by name
+      language:LANGUAGE --> rails language:javascript matches repos with the word "rails" that are written in JS
+      Matthew Coolest in:name language:javascript
+      https://api.github.com/search/repositories?q=jquery%20in%3Aname%20sort%3Dstart%20order-desc
+    */
+    octokit.request('GET /search/repositories', {
+      q: 'jquery in:name&sort=start&order-desc'
+    })
+    .then((res) => {
+      console.log(res.data.items);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    
+   
   }
+
+  return (
+    <div className='App'>
+      Search:<br/>
+      <input type='text' onChange={(e) => setInput(e.target.value)}></input>
+      <button onClick={searchSubmit}>Submit</button>
+    </div>
+  );
+ 
 }
 
 export default App;
